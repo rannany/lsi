71930b929bbc905b5b875f3db9a597e93d43906a
rannany <rannany@gmail.com>
2018-05-11 08:44:07 -0300

    refatoção dos modelos do banco

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, Table
 from sqlalchemy import create_engine
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
@@ -8,16 +8,27 @@ engine = create_engine('postgresql://postgres:admin@localhost/lsi')
 Base = declarative_base()
 
 
-class Word(Base):
+class DocumentTerm(Base):
 
-    __tablename__ = 'words'
-    id = Column(Integer, primary_key=True)
-    word = Column(String)
-    document_id = Column(Integer, ForeignKey('documents.id'))
-    document = relationship("Document", back_populates="words")
+    __tablename__ = 'document_term'
+    id = Column(Integer, primary_key=True, unique=True)
+    term_id = Column(Integer, ForeignKey('terms.id'), primary_key=True),
+    document_id = Column(Integer, ForeignKey('documents.id'), primary_key=True),
+    Column('frequency', Integer)
+    term = relationship("Document", back_populates="terms")
+    document = relationship("Term", back_populates="documents")
+
+
+class Term(Base):
+
+    __tablename__ = 'terms'
+    id = Column(Integer, unique=True, primary_key=True)
+    term = Column(String, unique=True)
+    frequency = Column(Integer)
+    document = relationship("DocumentTerm", back_populates="terms")
 
     def __repr__(self):
-        return self.word
+        return self.term
 
 
 class Document(Base):
@@ -28,7 +39,8 @@ class Document(Base):
     url = Column(String)
     created_at = Column(Date)
     updated_at = Column(Date)
-    words = relationship("Word", back_populates="document")
+    term = relationship("DocumentTerm", back_populates="documents")
 
     def __repr__(self):
        return self.title
+
